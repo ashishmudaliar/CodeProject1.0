37fa7732c2dbcf65c128955e79ec58fdbef50a3b
Ashish <ashish.mudaliar@yahoo.com>
2016-04-18 22:08:20 +0200

    Updated configuration to generate executable jar

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,7 @@
 package sample;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Ashish
  *
  */
+@EnableAutoConfiguration
 @SpringBootApplication
 public class Main {
 
